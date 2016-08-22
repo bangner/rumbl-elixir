4939296dd03f7da0551de03c65cd70f2c19b6acc
Joe Wagner <joe@mobilexlabs.com>
2016-08-22 09:54:04 -0500

    come back to login function

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,6 +1,15 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
 
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
+
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username), [])
@@ -23,15 +32,4 @@ defmodule Rumbl.User do
         changeset
     end
   end
-
-  schema "users" do
-    field :name, :string
-    field :username, :string
-    field :password, :string, virtual: true
-    field :password_hash, :string
-
-    timestamps
-  end
-
-
 end
